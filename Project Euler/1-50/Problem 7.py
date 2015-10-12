2bd5b3139ccce4f1120d120d10dfb5e2d1d189a4
Jeremy Sandberg <Jeremy.sandbergtobelem@itggot.se>
2015-10-12 13:10:35 +0200

    Improved Problem 7

--- Project Euler/1-50/Problem 7.py
+++ Project Euler/1-50/Problem 7.py
@@ -2,12 +2,9 @@ prime_cache = []
 def nth_prime(n):
     curr = 2
     while n >= len(prime_cache):
-        if any(curr%prime == 0 for prime in prime_cache):
-            curr += 1
-        else:
-            if curr not in prime_cache:
-                prime_cache.append(curr)
-            curr +=1
+        if not any(curr%prime == 0 for prime in prime_cache) and curr not in prime_cache:
+            prime_cache.append(curr)
+        curr +=1
     return prime_cache[n-1]
 
 print nth_prime(n=10001)
